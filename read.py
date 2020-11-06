041fa76b0d3d0b2b9e91b662a9c6db75da3757a5
mathias <mathias.winther@gmail.com>
2020-11-06 16:57:28 +0100

    read temperature too, and allow force extraction

--- read.py
+++ read.py
@@ -1,7 +1,32 @@
+import os
 import time
 import numpy as np
 
 import rotations
+
+
+times = ["morning", "noon", "afternoon", "evening"]
+poses = ["verticalUp", "elevatedapprox45", "parallel", "downapprox45", "verticalDown"]
+
+
+def get_file_path(dirname, time, pose):
+    """ Using our naming convention, construct a file path. """
+    
+    if type(time) in range(len(times)):
+        time = times[time]
+    else:
+        assert time in times
+
+    if type(pose) in range(len(poses)):
+        pose = poses[pose]
+    else:
+        assert pose in poses
+
+    dirpath = os.path.join("data", dirname)
+    tail = "rotateby10_posDirection_fixedPose"
+    filename = "ur10e_%s_%s_%s.csv" % (time, pose, tail)
+    
+    return os.path.join(dirpath, filename)
 
 
 def text2dict(text):
@@ -63,19 +88,43 @@ def extract_joint_angles(data):
     return np.stack(columns, axis=1)
 
 
+def extract_joint_temp(data):
+    """ Extract the joint temperatures in degrees. """
+
+    names = ["joint_temperatures_%s" % i for i in range(6)]
+    columns = [data[name] for name in names]
+
+    return np.stack(columns, axis=1)
+
 
 class Recording:
 
     def __init__(self, csvpath):
 
+        dirpath = os.path.dirname(csvpath)
+        if not os.path.isdir(dirpath):
+            raise FileNotFoundError("Dir %r does not exist" % dirpath)
+        elif not os.path.isfile(csvpath):
+            print("Requested file name: %r" % os.path.basename(csvpath))
+            print("Requested dir path: %r" % dirpath)
+            print("Dir contents:")
+            for filename in os.listdir(dirpath):
+                print(filename)
+            print()
+            raise FileNotFoundError("%r not in %r" % (csvpath, dirpath))
+
         start = time.perf_counter()
         print("Reading record at %r . . ." % csvpath)
+
+        self.path = csvpath
 
         with open(csvpath, "r") as source:
             self.data = text2dict(source.read())
 
         dur = time.perf_counter() - start
         print("Done after %.3f seconds.\n" % dur)
+
+        self.temp = extract_joint_temp(self.data)
 
         self.force = extract_tcp_forces(self.data)
         self.force_xyz, self.force_rvec = np.split(self.force, 2, axis=1)
@@ -86,6 +135,9 @@ class Recording:
         self.raw_force_xyz, self.raw_force_rvec = np.split(self.raw_force, 2, axis=1)
         self.raw_force_rmats = rotations.rvecs2matrices(self.raw_force_rvec)
         self.raw_force_euler = rotations.matrix2euler(self.raw_force_rmats)
+
+        mean_raw_xyz = np.mean(self.raw_force_xyz[:250, :], axis=0)
+        self.centered_raw_xyz = self.raw_force_xyz - mean_raw_xyz
 
         self.pose = extract_tcp_poses(self.data)
         self.pose_xyz, self.pose_rvec = np.split(self.pose, 2, axis=1)
@@ -119,6 +171,8 @@ class Recording:
         self.raw_force_rmats = self.raw_force_rmats[still, :]
         self.raw_force_euler = self.raw_force_euler[still, :]
 
+        self.centered_raw_xyz = self.centered_raw_xyz[still, :]
+
         self.pose = self.pose[still, :]
         self.pose_xyz = self.pose_xyz[still, :]
         self.pose_rvec = self.pose_rvec[still, :]
@@ -136,3 +190,15 @@ class Recording:
         print("Removed %s / %s frames.\n" % (sum(~still), len(still)))
 
         return self
+    
+    def get_base_frame_xyz_force(self, force_type="ACTUAL"):
+        """ Extract either the actual or raw xyz forces. """
+
+        if "ACTUAL" in force_type.upper():
+            rmats = self.pose_rmats.transpose((0, 2, 1))
+            colvecs = self.force_xyz[:, :, None]
+            return np.squeeze(rmats @ colvecs, axis=2)
+        elif "RAW" in force_type.upper():
+            return self.centered_raw_xyz
+        else:
+            raise ValueError("%r not in ['ACTUAL', 'RAW']" % force_type)
